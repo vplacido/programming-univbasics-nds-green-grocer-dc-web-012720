75eb69cf9c6d2c233155be710c338f4e627bc003
Valentin Placido <valentinplacido25@gmail.com>
2020-01-23 01:22:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -34,6 +34,25 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  coupons.each do |coupon|
+  #	    item_info = find_item_by_name_in_collection(coupon[:item], cart)
+  # REMEMBER: This method **should** update cart		  item_w_coupon = find_item_by_name_in_collection(coupon[:item]+" W/COUPON", cart)
+end	    if item_w_coupon and item_info[:count] >= coupon[:num]
+	    item_w_coupon[:count] += coupon[:num]
+	    item_info[:count] -= coupon[:num]
+	  elsif item_info and item_info[:count] >= coupon[:num]
+      cart << {
+        :item => coupon[:item] + " W/COUPON",
+        :price => (coupon[:cost]/coupon[:num]).round(2),
+        :clearance => item_info[:clearance],
+        :count => coupon[:num]
+      }
+      item_info[:count] -= coupon[:num]
+    end #if
+  end #each
+  #cart.delete_if{|item_info| item_info[:count] <= 0}
+  cart
+end #method apply_coupons
   newArr = []
   i = 0 
   while i < coupons.length do 
