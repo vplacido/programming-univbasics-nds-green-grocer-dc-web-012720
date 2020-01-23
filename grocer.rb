1ef22ca3271a47336424b91aa0cee10f3871d3db
Valentin Placido <valentinplacido25@gmail.com>
2020-01-23 01:15:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,7 +41,7 @@ def apply_coupons(cart, coupons)
     newItem = find_item_by_name_in_collection(coupons[i][:item]+" W/COUPON", cart)
     if newItem and item[:count] >= coupons[i][:num]
       newItem[:count] += coupons[i][:num]
-      
+      item[:count] -= coupons[i][:num]
   
   newArr
 end
